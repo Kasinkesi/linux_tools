97c632d979cb664a1d6ebf2d9998aa7e93be674c
doka <razdolbay89@gmail.com>
2022-10-17 13:59:43 +0300

    -b -n parameters support

--- cat/api/cli.py
+++ cat/api/cli.py
@@ -9,14 +9,14 @@ from bin import main
 parser = argparse.ArgumentParser(description="cat.py - объединяет файлы и направляет их на стандартный вывод.")
 parser.add_argument("files", nargs="*", default=sys.stdin,
                     help="""перечисление файлов для чтения(объеинения). Если ФАЙЛ не указан или вместо его имени стоит дефис (-), то производится чтение со стандартного ввода.""")
-parser.add_argument("-b", "--number-nonblank", action="store_true",
+parser.add_argument("-b", "--number-nonblank", dest="number_nonblank", action="store_true",
                     help="нумерует ВСЕ НЕПУСТЫЕ строки выходного файла, начиная c 1")
 parser.add_argument("-n", "--number", action="store_true",
                     help="нумерует ВСЕ строки выходного файла, начиная c 1")
 args = parser.parse_args()
 
 if args.files == sys.stdin:
-    main.steam_reader(sys.stdin)
+    main.steam_reader(sys.stdin, args.number_nonblank, args.number)
 
 else:
-    main.files_reader(args.files)
+    main.files_reader(args.files, args.number_nonblank, args.number)
--- cat/bin/main.py
+++ cat/bin/main.py
@@ -1,18 +1,40 @@
 import sys
 
 
-def steam_reader(input_stream):
+def steam_reader(input_stream, b, n):
     for line in input_stream:
-        sys.stdout.write(line)
-        # print(line, end="")
+        if b:
+            sys.stdout.write(number_nonblank(line))
+        elif n:
+            sys.stdout.write(number(line))
+        else:
+            sys.stdout.write(line)
 
 
-def files_reader(files_list):
+def files_reader(files_list, b, n):
     for file in files_list:
         try:
             with open(file, "r", errors='replace') as src:
-                steam_reader(src)
+                steam_reader(src, b, n)
         except FileNotFoundError:
             print(f"cat: {file}: Нет такого файла или каталога")
         except IsADirectoryError:
             print(f"cat: {file}: Это каталог")
+
+
+def number(line, n=[]):
+    if n == []:
+        n.append(1)
+    else:
+        n[0] += 1
+    return "    " + str(n[0]) + " " + line
+
+
+def number_nonblank(line, n=[]):
+    if n == [] and line != "\n":
+        n.append(1)
+    elif n != [] and line != "\n":
+        n[0] += 1
+    else:
+        return line
+    return "    " + str(n[0]) + " " + line
