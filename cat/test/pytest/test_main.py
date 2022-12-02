1f20274e14bd53900138a885f23a1a03e0034a11
doka <razdolbay89@gmail.com>
2022-12-02 19:05:28 +0300

    test_main parametrise

--- cat/test/pytest/test_main.py
+++ cat/test/pytest/test_main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pytest
 from collections import namedtuple
 
 try:
@@ -9,21 +10,35 @@ except:
     sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
     from bin import main
 
+flags_and_expect_list = [((0, 0, 0), "hi\n\nhello8)\n"),
+                         ((0, 0, 1), "hi\n\nhellono smile for the wicked\n"),
+                         ((0, 1, 0), "     1  hi\n     2  \n     3  hello8)\n"),
+                         ((0, 1, 1), "     1  hi\n     2  \n     3  hellono smile for the wicked\n"),
+                         ((1, 0, 0), "     1  hi\n\n     2  hello8)\n"),
+                         ((1, 0, 1), "     1  hi\n\n     2  hellono smile for the wicked\n"),
+                         ((1, 1, 0), "     1  hi\n\n     2  hello8)\n"),
+                         ((1, 1, 1), "     1  hi\n\n     2  hellono smile for the wicked\n")]
 
-def test_default_files(monkeypatch, nonempty_stream, capsys):
+
+def idfn(val):
+    if isinstance(val, tuple):
+        return str(val)
+
+
+@pytest.mark.parametrize('flags, expect', flags_and_expect_list, ids=idfn)
+def test_default_files_and_variable_flags(flags, expect, monkeypatch, nonempty_stream, capsys):
     monkeypatch.setattr('sys.stdin', nonempty_stream)
 
     def args_gen():
         args_tuple = namedtuple('args', ['files', 'number_nonblank', 'number', 'smile_exterminator'])
-        return args_tuple(sys.stdin, 1, 1, 1)
+        return args_tuple(sys.stdin, *flags)
 
     monkeypatch.setattr(main, "parse_args", args_gen)
 
     main.main()
 
     captured = capsys.readouterr()
-    expected = '     1  hi\n\n     2  hellono smile for the wicked\n'
-    assert captured.out == expected
+    assert captured.out == expect
 
 
 def test_files(monkeypatch, files_list_gen, capsys):
